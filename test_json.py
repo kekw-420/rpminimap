e753fad166a3f87f63f5297396bcfc88c2c148b8
George <goblin.faces@gmail.com>
2020-02-15 23:08:00 +0000

    Added image exist test

--- test_json.py
+++ test_json.py
@@ -1,4 +1,5 @@
 import json
+import requests
 
 # Checks for JSONDecode Errors, finds shit like missing brackets
 def syntax_check():
@@ -20,8 +21,26 @@ def content_check():
         if key != image:
             raise AssertionError
 
+# Checks that all the image paths given actually exist
+def image_check():
+    url = "https://raw.githubusercontent.com/traa-pixelcanvas/Minimap/master/templates/"
+    data = None
+    with open("templates/!data.json") as json_file:
+        data = json.loads(json_file.read())
+    templates = data["Templates"]
+
+    for key, value in templates.items():
+        image_path = value["name"]
+        image_path = f"{url}{image_path}"
+        response = requests.get(image_path)
+        if response.status_code != 200:
+            raise AssertionError
+
 def test_syntax():
     syntax_check()
 
 def test_content():
     content_check()
+
+def test_image():
+    image_check()
